7dfc8c07e3c74c534d7bb8b0676397dad012132c
serbrio <28647145+serbrio@users.noreply.github.com>
2018-09-08 01:06:07 +0300

    add

--- week5/socket_serv.py
+++ week5/socket_serv.py
@@ -6,9 +6,14 @@ with socket.socket() as sock:
     sock.listen()
     while True:
         conn, addr = sock.accept()
+        conn.settimeout(5)    # timeout := None|0|gt 0
         with conn:
             while True:
-                data = conn.recv(1024)
+                try:
+                    data = conn.recv(1024)
+                except socket.timeout:
+                    print('close connection by timeout')
+                    break
                 if not data:
                     break
                 print(data.decode("utf8"))
