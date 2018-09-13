830c28bdd37990dafcbe2681a2a57a50ac97d6f0
sergey <s.britvin@corp.mail.ru>
2018-09-13 21:08:42 +0300

    fin w5

---
+++ week5/metrics_client.py
@@ -0,0 +1,17 @@
+import socket
+import time
+
+
+class Client():
+    def __init__(self, host, port, timeout=None):
+        self.timeout = timeout or
+
+    def get(self):
+        pass
+
+    def put(self):
+        pass
+
+    @staticmethod
+    def _get_timestamp():
+        return str(int(time.time()))
