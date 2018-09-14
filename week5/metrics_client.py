8af162872467dd4de43c635417b1490972813e86
sergey <s.britvin@corp.mail.ru>
2018-09-14 20:19:41 +0300

    s

--- week5/metrics_client.py
+++ week5/metrics_client.py
@@ -4,14 +4,26 @@ import time
 
 class Client():
     def __init__(self, host, port, timeout=None):
-        self.timeout = timeout or
-
-    def get(self):
-        pass
-
-    def put(self):
-        pass
+        self.host = host
+        self.port = port
+        self.timeout = timeout
 
     @staticmethod
-    def _get_timestamp():
+    def get_timestamp():
         return str(int(time.time()))
+
+    def get(self, key):
+        return {}
+        pass
+
+    def put(self, key, value, timestamp=None):
+        timestamp = timestamp or self.get_timestamp()
+        print('put key %s and value %s at time %s' % (key, value, timestamp))
+        pass
+
+
+class ClientError(Exception):  # hope to use it
+    pass
+
+
+
